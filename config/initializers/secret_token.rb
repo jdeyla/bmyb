ba96bf83fa58f6e1cc6e0a639dee4b1f7d6337c4
Jacopo Deyla <ipa@MacBook-Pro-di-jacopo-deyla.local>
2011-06-20 22:00:53 +0200

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bmyb::Application.config.secret_token = '411ecfeaf2734b0a2c81c62f77fce5c6080e76d27c580b099b88b6eeda839fb279beb84b064cadb126e18b092c644d8796b9cba292da646656a1e999e92e97cb'
