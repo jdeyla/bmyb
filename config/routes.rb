0e71d528db5f7dd191cd5d60ab54558d54689ad3
Jacopo Deyla <jdeyla@gmail.com>
2011-06-27 16:14:49 +0200

    git gem install e welcome route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Bmyb::Application.routes.draw do
 
+  get "welcome/index"
+
   devise_for :users
 
   #devise
-  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,7 +55,7 @@ Bmyb::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
